b0962446e983bc2fc5cc06c69436c29351b5fdef
Alex Blair <bclan11@gmail.com>
2022-05-17 15:44:58 -0700

    building in Home before refactor.  List, Details, and Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Auth from './views/Auth/Auth';
 import Home from './views/Home/Home';
-import ContextProvider from './context/ContextProvider';
+// import ContextProvider from './context/ContextProvider';
 
 export default function App() {
   return (
-    <ContextProvider>
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Auth />
-          </Route>
-          <Route path="/register">
-            <Auth />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </ContextProvider>
+    // <ContextProvider>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Auth />
+        </Route>
+        <Route path="/register">
+          <Auth />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+    // {/* </ContextProvider> */}
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,55 @@
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <h1>ironicgram</h1>
+        <h3>username</h3>
+        <button>signout</button>
+      </header>
+      <main>
+        <Router>
+          <Link to="/create">
+            <button>create</button>
+          </Link>
+          <Switch>
+            <Route exact path="/create">
+              <h1>create</h1>
+            </Route>
+            <Route path="/details">
+              <h1>details</h1>
+            </Route>
+            <Route>
+              <div className="listView">
+                <ul>
+                  <Link to="/details/:id">
+                    <p>user author</p>
+                    <img src="https://www.placecage.com/c/100/100" alt="" />
+                    <p>caption</p>
+                  </Link>
+                  <Link>
+                    <p>user author</p>
+                    <img src="https://www.placecage.com/c/100/101" alt="" />
+                    <p>caption</p>
+                  </Link>
+                  <Link>
+                    <p>user author</p>
+                    <img src="https://www.placecage.com/c/100/102" alt="" />
+                    <p>caption</p>
+                  </Link>
+                </ul>
+              </div>
+            </Route>
+          </Switch>
+        </Router>
+      </main>
+    </>
+  );
 }
