39ae90709b978f29902609d068441a4e16499d99
Alex Blair <bclan11@gmail.com>
2022-05-18 10:45:21 -0700

    supabase modules error. Gonna try pushing, deleting locally, and cloning down?

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Auth from './views/Auth/Auth';
 import Home from './views/Home/Home';
-// import ContextProvider from './context/ContextProvider';
+import { UserProvider } from './context/UserContext';
 
 export default function App() {
   return (
-    // <ContextProvider>
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Auth />
-        </Route>
-        <Route path="/register">
-          <Auth />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-    // {/* </ContextProvider> */}
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Auth />
+          </Route>
+          <Route path="/register">
+            <Auth />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 }
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState } from 'react';
+import {
+  getUser,
+  signInUser,
+  signUpUser,
+  signOutUser,
+} from '../services/users';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+
+  const [user, setUser] = useState(
+    currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+  );
+
+  const value = { user, setUser };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+export const useAuth = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a UserProvider');
+  }
+  const { user, setUser } = context;
+  const isLoggedIn = user?.email;
+
+  const signUp = async (email, password) => {
+    const user = await signUpUser(email, password);
+    setUser(user);
+  };
+  const signIn = async (email, password) => {
+    const user = await signInUser(email, password);
+    setUser(user);
+  };
+
+  const signOut = async () => {
+    await signOutUser();
+    setUser({});
+  };
+
+  return { user, isLoggedIn, signUp, signIn, signOut };
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used withina UserProvider');
+  }
+  const { user } = context;
+
+  return { user };
+};
