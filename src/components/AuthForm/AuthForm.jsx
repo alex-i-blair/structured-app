c59be8651051f4b1d418f358c63f1f2885ee7e22
Alex Blair <bclan11@gmail.com>
2022-05-18 16:01:40 -0700

    auth form and auth view in the works. need to hook it up

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+export default function AuthForm({ label, onSubmit, inputs = {} }) {
+  // const useForm = (inputs = {}) => {
+  // const [formState, setFormState] = useState(inputs);
+  //
+  // };
+  const [formState, setFormState] = useState(inputs);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = formState;
+
+    try {
+      setLoading(true);
+      await onSubmit(email, password);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>{label}</legend>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formState.email}
+            required
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formState.password}
+            minLength="8"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit" disabled={loading}>
+            {loading ? 'Loading...' : label}
+          </button>
+        </div>
+      </fieldset>
+    </form>
+  );
+}
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,3 +1,41 @@
-export default function Auth() {
-  return <div>Auth</div>;
+import {
+  Link,
+  Redirect,
+  useHistory,
+} from 'react-router-dom/cjs/react-router-dom.min';
+import AuthForm from '../../components/AuthForm/AuthForm';
+import { useAuth } from '../../context/UserContext';
+
+export default function Auth({ isSigningUp = false }) {
+  const history = useHistory();
+  const { isLoggedIn, signUp, signIn } = useAuth();
+
+  const signUpOptions = {
+    action: signUp,
+    label: 'Sign Up',
+    message: <Link to="/login">Sign in</Link>,
+  };
+  const signInOptions = {
+    action: signIn,
+    redirectTo: '/',
+    label: 'Sign in',
+    message: <Link to="/register">Sign up</Link>,
+  };
+
+  const options = isSigningUp ? signUpOptions : signInOptions;
+
+  const handleAuthSubmit = async (email, password) => {
+    await options.action(email, password);
+    history.replace(options.redirectTo);
+  };
+
+  if (isLoggedIn) return <Redirect to="/" />;
+
+  return (
+    <>
+      <div className="authForm">
+        <AuthForm onSubmit={handleAuthSubmit} label={options.label} />
+      </div>
+    </>
+  );
 }
